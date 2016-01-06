63253a14dbd60c137d6cb7c3943bd9c3083957c5
Abdelrahman Mohamed <obada.semary@gmail.com>
2016-01-06 12:54:12 +0300

    1.1
    data
    response
    parsedResult
    stat
    photosDictionary

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -8,18 +8,141 @@
 
 import UIKit
 
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flickr.photos.search"
+//let API_KEY = "8162cbed138466b501453381c1ce5bc9"
+//let API_KEY = "ce190e05b11ca689a9clfac8c9de619d"
+let API_KEY = "7ec0be775569078cb3893cccf909019f"
+let EXTRAS = "url_m"
+let SAFE_SEARCH = "1"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var defaultLabel: UILabel!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+
+    @IBAction func searchPhotosByPhraseButtonTouchUp(sender: AnyObject) {
+        /* 1 - Hardcode the arguments */
+        let methodArguments: [String: String!] = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "text": "realmadrid",
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        /* 2 - Call the Flickr API with these arguments */
+        getImageFromFlickrBySearch(methodArguments)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
+    
     }
+    
+    // MARK: Flickr API
+    
+    func getImageFromFlickrBySearch(methodArguments: [String : AnyObject]) {
+        
+        /* 3 - Get the shared NSURLSession to faciliate network activity */
+        let session = NSURLSession.sharedSession()
+        
+        /* 4 - Create the NSURLRequest using properly escaped URL */
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
 
+        /* 5 - Create NSURLSessionDataTask and completion handler */
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
 
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find keys 'photos' in \(parsedResult)")
+                return
+            }
+
+            print(data)
+            print("\n************************************************************************************************************\n")
+            print(response)
+            print("\n************************************************************************************************************\n")
+            print(parsedResult)
+            print("\n************************************************************************************************************\n")
+            print(stat)
+            print("\n************************************************************************************************************\n")
+            print(photosDictionary)
+            
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: Escape HTML Parameters
+    
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+    }
 }
 
